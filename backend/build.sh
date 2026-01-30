0b164082241da6562da45c14898ee27ecfe441d6
Chitraksh Sharma <chitraksh73278@gmail.com>
2026-01-30 14:53:47 +0530

    Add auto-superuser

--- backend/build.sh
+++ backend/build.sh
@@ -7,3 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', '', 'admin123')"
